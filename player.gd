a17073940ea86044068766fa5048fcbb12588403
addyvmc20-ctrl <addyvmc20@gmail.com>
2026-05-11 18:03:38 +1200

    Ice platform, movement and level improvement

--- player.gd
+++ player.gd
@@ -5,8 +5,12 @@ var twist_input := 0.0
 var pitch_input := 0.0
 
 @export var jump_force := 12.0
+@export var move_force := 1200.0
+@export var air_move_force := 500.0
+@export var air_damp := 1.0
 
 var can_jump := false
+var touching_ice := false
 
 @onready var twist_pivot := $TwistPivot
 @onready var pitch_pivot := $TwistPivot/PitchPivot
@@ -18,12 +22,28 @@ func _process(delta: float) -> void:
 	var input := Vector3.ZERO 
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_back")
-	
-	apply_central_force(twist_pivot.basis * input * 1200.0 * delta)
-	
+
+	if touching_ice:
+		linear_damp = 0.1
+	elif can_jump:
+		linear_damp = 3.0
+	else:
+		linear_damp = air_damp
+
+	var current_move_force := move_force
+
+	if not can_jump:
+		current_move_force = air_move_force
+
+	apply_central_force(twist_pivot.basis * input * current_move_force * delta)
+
 	twist_pivot.rotate_y(twist_input)
 	pitch_pivot.rotate_x(pitch_input)
-	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, deg_to_rad(-30), deg_to_rad(30))
+	pitch_pivot.rotation.x = clamp(
+		pitch_pivot.rotation.x,
+		deg_to_rad(-30),
+		deg_to_rad(30)
+	)
 
 	twist_input = 0.0
 	pitch_input = 0.0
@@ -43,10 +63,14 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _integrate_forces(state):
 	can_jump = false
+	touching_ice = false
 
 	for i in range(state.get_contact_count()):
 		var normal = state.get_contact_local_normal(i)
+		var body = state.get_contact_collider_object(i)
 
 		if normal.dot(Vector3.UP) > 0.7:
 			can_jump = true
-			break
+
+			if body != null and body.is_in_group("ice"):
+				touching_ice = true
