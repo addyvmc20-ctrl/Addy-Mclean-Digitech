952e84d5c051c12c11615be44587cf63be8ebd05
addyvmc20-ctrl <addyvmc20@gmail.com>
2026-06-03 20:25:04 +1200

    Level 4 creation

--- Scripts/level_menu.gd
+++ Scripts/level_menu.gd
@@ -21,3 +21,7 @@ func _on_level_2_pressed() -> void:
 
 func _on_level_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Level_3.tscn")
+
+
+func _on_level_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_4.tscn")
