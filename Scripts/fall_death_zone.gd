8851c0cc9fac1685df0e17293278aabdd29c6c49
addyvmc20-ctrl <addyvmc20@gmail.com>
2026-09-19 10:29:06 +1200

    DeathcountHUD_Crystals on all levels

--- Scripts/fall_death_zone.gd
+++ Scripts/fall_death_zone.gd
@@ -2,17 +2,22 @@ extends Area3D
 
 @export var respawn_position := Vector3(0, 3, 0)
 
+var death_count := 0
+
 func _on_body_entered(body):
 	if body.name == "Player":
+		# Add to death count
+		death_count += 1
+		
+		# Update death counter GUI
+		$"../DeathCountHUD/Label".text = "Deaths: " + str(death_count)
+		
+		# Reset player
 		body.global_position = respawn_position
 		body.linear_velocity = Vector3(0, 0, 0)
 		body.angular_velocity = Vector3(0, 0, 0)
-
+		
 		# Reset crystal HUD
 		var crystal_hud = get_tree().get_first_node_in_group("crystal_hud")
 		if crystal_hud:
 			crystal_hud.reset_crystals()
-
-		# Reset all crystals in the level
-		for crystal in get_tree().get_nodes_in_group("crystals"):
-			crystal.reset_crystal()
