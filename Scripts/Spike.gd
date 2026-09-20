697097ca62f5a78a138c7fcc30065e89be89d38c
addyvmc20-ctrl <addyvmc20@gmail.com>
2026-09-20 12:09:42 +1200

    Finished all planets and solar system - organsied nodes - polished levels and file systems

--- Scripts/Spike.gd
+++
@@ -1,9 +0,0 @@
-extends Area3D
-
-@export var respawn_position := Vector3(0, 3, 0)
-
-func _on_body_entered(body):
-	if body.name == "Player":
-		body.global_position = respawn_position
-		body.linear_velocity = Vector3(0, 0, 0)
-		body.angular_velocity = Vector3(0, 0, 0)
--- pause_menu.gd
+++ pause_menu.gd
@@ -2,7 +2,6 @@ extends CanvasLayer
 
 func _ready():
 	visible = false
-	process_mode = Node.PROCESS_MODE_ALWAYS
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
@@ -11,22 +10,22 @@ func _input(event):
 func toggle_pause():
 	visible = !visible
 	get_tree().paused = visible
-	
-	if visible:
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	else:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+
+
 
 func _on_resume_pressed() -> void:
 	get_tree().paused = false
 	visible = false
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 
 func _on_levels_pressed() -> void:
 	get_tree().paused = false
 	visible = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	get_tree().change_scene_to_file("res://Scenes/level_menu.tscn")
+
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
